23097912e08a88f42e2a79cf362275f1b78cf211
osvaldopina <osvaldo.pina@gmail.com>
2022-11-18 18:21:58 +0000

    more on service starter

--- src/test_containers/main.py
+++ src/test_containers/main.py
@@ -1,21 +1,23 @@
-
 import click
 
 
 @click.group()
+# @click.option('--file', '-f', type=str, help="compose file")
 def cli():
     pass
 
 
 @click.command(name="show-status")
-def show_status():
+@click.option('--file', '-f', required=True, type=str, help="compose file")
+def show_status(file):
     """show services status and dependencies"""
     print('show-status')
 
 
 @click.command(name="start")
+@click.option('--file', '-f', required=True, type=str, help="compose file")
 @click.option('--until')
-def start():
+def start(file):
     """start services and run exec container"""
     print('start')
 
